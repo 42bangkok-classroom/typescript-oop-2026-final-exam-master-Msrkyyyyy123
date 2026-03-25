56e7576af4192a24c4681862a9fec9f42cf3374a
event <event@z2t12c2.42bangkok.com>
2026-03-25 10:25:21 +0700

    aaa

--- src/products/product.interface.ts
+++ src/products/product.interface.ts
@@ -5,4 +5,3 @@ export interface Product {
     stock: number;
     description: string;
   }
-  
--- src/products/product.module.ts
+++ src/products/product.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { APP_FILTER } from '@nestjs/core';
+// import { APP_FILTER } from '@nestjs/core';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 
